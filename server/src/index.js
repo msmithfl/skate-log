bcb6138b44ea7b6572a07b9f4bcffbc964f10797
Matthew Smith <msmithfl@icloud.com>
2023-05-04 16:42:28 -0400

    User routes working

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import { userRouter } from "./routes/users.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect(
   "mongodb+srv://msmithfl:2Pw2LHotPsQUrWQR@skate-log.iqtsw2g.mongodb.net/skate-log?retryWrites=true&w=majority"
