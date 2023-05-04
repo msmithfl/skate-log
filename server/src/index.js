545ac4921959dd7d9f83afa000308aba79e34059
Matthew Smith <msmithfl@icloud.com>
2023-05-04 16:05:55 -0400

    rename skate-log-app to client

--- server/src/index.js
+++ server/src/index.js
@@ -7,4 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(
+  "mongodb+srv://msmithfl:2Pw2LHotPsQUrWQR@skate-log.iqtsw2g.mongodb.net/skate-log?retryWrites=true&w=majority"
+);
+
 app.listen(3001, () => console.log("SERVER STARTED!"));
