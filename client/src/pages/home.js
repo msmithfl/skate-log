934f83a5320c61bc4d7f2f89f5b1efa49191166f
Matthew Smith <msmithfl@icloud.com>
2023-05-04 19:48:10 -0400

    homepage and landed page setup

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
 
 const Home = () => {
-  return <div className="text-3xl">Home</div>;
+  const [tricks, setTricks] = useState([]);
+  const [completedTricks, setCompletedTricks] = useState([]);
+
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchTricks = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/tricks");
+        setTricks(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    const fetchCompletedTricks = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/tricks/completedTricks/ids/${userID}`
+        );
+        setCompletedTricks(response.data.completedTricks);
+        console.log(completedTricks);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchTricks();
+    fetchCompletedTricks();
+  }, []);
+
+  const saveTrick = async (event, trickID) => {
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      try {
+        await axios.put("http://localhost:3001/tricks", {
+          trickID,
+          userID,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      try {
+        await axios.delete("http://localhost:3001/tricks", {
+          data: { userID: userID, trickID: trickID },
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
+  const strikeoutText = () => {
+    console.log("FIRED");
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center m-2">Tricklist</h1>
+      <ul>
+        {tricks.map((trick) => (
+          <li key={trick._id}>
+            <div className="flex justify-between items-center mx-8 gap-2">
+              <input
+                type="checkbox"
+                onClick={(event) => {
+                  saveTrick(event, trick._id);
+                  strikeoutText();
+                }}
+              />
+              <h2 className="text-xl">{trick.name}</h2>
+              <i className="fa-regular fa-heart"></i>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
--- client/src/pages/landed.js
+++ client/src/pages/landed.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
 
 const Landed = () => {
-  return <div className="text-3xl">Landed</div>;
+  const [completedTricks, setCompletedTricks] = useState([]);
+
+  const userID = useGetUserID();
+
+  // getting the user's completed tricks
+  useEffect(() => {
+    const fetchCompletedTricks = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/tricks/completedTricks/${userID}`
+        );
+        setCompletedTricks(response.data.completedTricks);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchCompletedTricks();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center m-2">My Progress</h1>
+      <ul>
+        {completedTricks.map((trick) => (
+          <li key={trick._id}>
+            <div className="flex justify-center">
+              <h2 className="text-xl">{trick.name}</h2>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Landed;
